0523fea7c986060f7d5c1525668b32609049a96b
Peechaya Mackenzie <tay_eee@hotmail.com>
2021-09-07 00:19:06 +0100

    Fixed Properties error and divided div in Cards

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,68 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Alert from "./Alert";
+import PropertyCard from "./PropertyCard";
+import SideBar from "./SideBar";
 
 const Properties = () => {
-  return <div>Properties Page</div>;
+  const initialState = {
+    properties: [],
+    alert: {
+      message: "",
+    },
+  };
+
+  const [properties, setProperties] = useState(initialState.properties);
+  const [alert, setAlert] = useState(initialState.alert);
+
+  useEffect(() => {
+    setAlert({ message: "" });
+
+    return axios
+      .get("http://localhost:4000/api/v1/PropertyListing")
+      .then((response) => setProperties(response.data))
+      .catch(() => {
+        setAlert({
+          message: "Properties are not found",
+        });
+      });
+  }, []);
+
+  const { search } = useLocation();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/PropertyListing${search}`)
+      .then(({ data }) => setProperties(data))
+      // eslint-disable-next-line no-console
+      .catch((err) => console.error(err));
+  }, [search]);
+
+  return (
+    <div className="properties">
+      <SideBar />
+      <Alert message={alert.message} />
+      {properties.map((property) => (
+        <div key={property._id} className="col">
+          <PropertyCard {...property} />
+        </div>
+      ))}
+      {/* {properties.map((property) => {
+        return (
+          <PropertyCard
+            key={property._id}
+            title={property.title}
+            type={property.title}
+            bathrooms={property.bathrooms}
+            bedrooms={property.bedrooms}
+            price={property.price}
+            city={property.city}
+            email={property.email}
+          />
+        );
+      })} */}
+    </div>
+  );
 };
 
 export default Properties;
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,28 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../styles/PropertyCard.css";
 
-const PropertyCard = ({ 
+const PropertyCard = ({
   title,
   type,
   bathrooms,
   bedrooms,
   price,
   city,
-  email
-  }) => {
+  email,
+}) => {
   return (
     <div className="property-card">
-      <ul>
-        <li className="property-card_title">{title}</li>
-        <li className="property-card_type">{type}</li>
-        <li className="property-card_bathrooms">{bathrooms}</li>
-        <li className="property-card_bedrooms">{bedrooms}</li>
-        <li className="property-card_price">{price}</li>
-        <li className="property-card_city">{city}</li>
-        <li className="property-card_email">{email}</li>
-      </ul>
+      {/* <div className="property-card_key">{key}</div> */}
+      <div className="property-card_title">{title}</div>
+      <div className="property-card_type">{type}</div>
+      <div className="property-card_bathrooms">{bathrooms}</div>
+      <div className="property-card_bedrooms">{bedrooms}</div>
+      <div className="property-card_price">{price}</div>
+      <div className="property-card_city">{city}</div>
+      <div className="property-card_email">{email}</div>
     </div>
   );
-}
+};
+
+PropertyCard.propTypes = {
+  // key: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  bedrooms: PropTypes.string.isRequired,
+  bathrooms: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
 
 export default PropertyCard;
